1e1a30a4c788c0ae492d2b265d9817afa4213c42
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-03-11 22:52:17 +0330

    format by ecs

--- src/RespinarWhatsappBundle.php
+++ src/RespinarWhatsappBundle.php
@@ -18,11 +18,7 @@ use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class RespinarWhatsappBundle extends AbstractBundle
 {
-    public function loadExtension(
-        array $config, 
-        ContainerConfigurator $containerConfigurator, 
-        ContainerBuilder $containerBuilder,
-    ): void
+    public function loadExtension(array $config, ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder,): void
     {
         $containerConfigurator->import('../config/services.yaml');
     }
