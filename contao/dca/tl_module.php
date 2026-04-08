06dbe6461f7c8c3c08383644807cca1db0e8e4ee
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-04-08 17:38:03 +0330

    format by ecs

--- contao/dca/tl_module.php
+++ contao/dca/tl_module.php
@@ -2,34 +2,34 @@
 
 declare(strict_types=1);
 
-$GLOBALS["TL_DCA"]["tl_module"]["palettes"]["whatsapp"] = '
+$GLOBALS['TL_DCA']['tl_module']['palettes']['whatsapp'] = '
     {title_legend},name,type;
     {whatsapp_legend},whatsappTitle,whatsappIsVisible,whatsappNumber,whatsappMessage;
     {template_legend:hide},cssID,customTpl;
     {protected_legend:hide},protected;
 ';
 
-$GLOBALS["TL_DCA"]["tl_module"]["fields"]["whatsappIsVisible"] = [
-    "exclude" => true,
-    "inputType" => "checkbox",
-    "eval" => ["tl_class" => "w50 m12"],
-    "sql" => ['type' => 'boolean', 'default' => true],
+$GLOBALS['TL_DCA']['tl_module']['fields']['whatsappIsVisible'] = [
+    'exclude' => true,
+    'inputType' => 'checkbox',
+    'eval' => ['tl_class' => 'w50 m12'],
+    'sql' => ['type' => 'boolean', 'default' => true],
 ];
 
-$GLOBALS["TL_DCA"]["tl_module"]["fields"]["whatsappTitle"] = [
-    "inputType" => "text",
-    "eval" => ["mandatory" => true, "maxlength" => 255, "tl_class" => "w50"],
-    "sql" => ['type' => 'string', 'length' => 255, 'default' => ''],
+$GLOBALS['TL_DCA']['tl_module']['fields']['whatsappTitle'] = [
+    'inputType' => 'text',
+    'eval' => ['mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50'],
+    'sql' => ['type' => 'string', 'length' => 255, 'default' => ''],
 ];
 
-$GLOBALS["TL_DCA"]["tl_module"]["fields"]["whatsappNumber"] = [
-    "inputType" => "text",
-    "eval" => ["mandatory" => true, "maxlength" => 20, "tl_class" => "w50 clr"],
-    "sql" => ['type' => 'string', 'length' => 20, 'default' => ''],
+$GLOBALS['TL_DCA']['tl_module']['fields']['whatsappNumber'] = [
+    'inputType' => 'text',
+    'eval' => ['mandatory' => true, 'maxlength' => 20, 'tl_class' => 'w50 clr'],
+    'sql' => ['type' => 'string', 'length' => 20, 'default' => ''],
 ];
 
-$GLOBALS["TL_DCA"]["tl_module"]["fields"]["whatsappMessage"] = [
-    "inputType" => "text",
-    "eval" => ["mandatory" => true, "maxlength" => 255, "tl_class" => "w50"],
-    "sql" => ['type' => 'string', 'length' => 255, 'default' => ''],
+$GLOBALS['TL_DCA']['tl_module']['fields']['whatsappMessage'] = [
+    'inputType' => 'text',
+    'eval' => ['mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50'],
+    'sql' => ['type' => 'string', 'length' => 255, 'default' => ''],
 ];
