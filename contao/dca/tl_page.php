eb873f5a07026c65e3198deecb6f4a99e92eefa3
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-04-08 17:38:06 +0330

    format by ecs

--- contao/dca/tl_page.php
+++ contao/dca/tl_page.php
@@ -1,41 +1,44 @@
 <?php
 
+declare(strict_types=1);
+
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
 PaletteManipulator::create()
-    ->addLegend("whatsapp_legend", "protected_legend", PaletteManipulator::POSITION_BEFORE)
-    ->addField(["whatsappTitle", "whatsappVisibility", "whatsappNumber", "whatsappMessage"], "whatsapp_legend", PaletteManipulator::POSITION_APPEND)
-    ->applyToPalette("regular", "tl_page")
+    ->addLegend('whatsapp_legend', 'protected_legend', PaletteManipulator::POSITION_BEFORE)
+    ->addField(['whatsappTitle', 'whatsappVisibility', 'whatsappNumber', 'whatsappMessage'], 'whatsapp_legend', PaletteManipulator::POSITION_APPEND)
+    ->applyToPalette('regular', 'tl_page')
     ->applyToPalette('root', 'tl_page')
-    ->applyToPalette('rootfallback', 'tl_page');
+    ->applyToPalette('rootfallback', 'tl_page')
+;
 
 // Define fields
-$GLOBALS["TL_DCA"]["tl_page"]["fields"]["whatsappVisibility"] = [
-    "exclude" => true,
-    "inputType" => "select",
-    'options'   => [ 'show', 'hide'],
-    'reference'=> &$GLOBALS['TL_LANG']['whatsappVisibilityOptions'],
-    "eval" => ['includeBlankOption' => true, "tl_class" => "w50"],
-    "sql" => ['type' => 'string', 'length' => 20, 'default' => ''],
+$GLOBALS['TL_DCA']['tl_page']['fields']['whatsappVisibility'] = [
+    'exclude' => true,
+    'inputType' => 'select',
+    'options' => ['show', 'hide'],
+    'reference' => &$GLOBALS['TL_LANG']['whatsappVisibilityOptions'],
+    'eval' => ['includeBlankOption' => true, 'tl_class' => 'w50'],
+    'sql' => ['type' => 'string', 'length' => 20, 'default' => ''],
 ];
 
-$GLOBALS["TL_DCA"]["tl_page"]["fields"]["whatsappTitle"] = [
-    "exclude" => true,
-    "inputType" => "text",
-    "eval" => ["maxlength" => 255, "tl_class" => "w50"],
-    "sql" => ['type' => 'string', 'length' => 255, 'default' => ''],
+$GLOBALS['TL_DCA']['tl_page']['fields']['whatsappTitle'] = [
+    'exclude' => true,
+    'inputType' => 'text',
+    'eval' => ['maxlength' => 255, 'tl_class' => 'w50'],
+    'sql' => ['type' => 'string', 'length' => 255, 'default' => ''],
 ];
 
-$GLOBALS["TL_DCA"]["tl_page"]["fields"]["whatsappNumber"] = [
-    "exclude" => true,
-    "inputType" => "text",
-    "eval" => ["rgxp" => "phone", "maxlength" => 20, "tl_class" => "w50"],
-    "sql" => ['type' => 'string', 'length' => 20, 'default' => ''],
+$GLOBALS['TL_DCA']['tl_page']['fields']['whatsappNumber'] = [
+    'exclude' => true,
+    'inputType' => 'text',
+    'eval' => ['rgxp' => 'phone', 'maxlength' => 20, 'tl_class' => 'w50'],
+    'sql' => ['type' => 'string', 'length' => 20, 'default' => ''],
 ];
 
-$GLOBALS["TL_DCA"]["tl_page"]["fields"]["whatsappMessage"] = [
-    "exclude" => true,
-    "inputType" => "text",
-    "eval" => ["maxlength" => 255, "tl_class" => "w50"],
-    "sql" => ['type' => 'string', 'length' => 255, 'default' => ''],
+$GLOBALS['TL_DCA']['tl_page']['fields']['whatsappMessage'] = [
+    'exclude' => true,
+    'inputType' => 'text',
+    'eval' => ['maxlength' => 255, 'tl_class' => 'w50'],
+    'sql' => ['type' => 'string', 'length' => 255, 'default' => ''],
 ];
